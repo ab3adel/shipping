d850a9f388b0fb2b23b5e78ef498ec09367d8388
Mohammad Ismaeel <50014764+ab3adel@users.noreply.github.com>
2022-01-03 19:09:13 +0200

    homepage1

--- src/components/home/home-section-four/home-section-four.tsx
+++ src/components/home/home-section-four/home-section-four.tsx
@@ -3,6 +3,12 @@ import './home-section-four.scss'
 import Wave from '../../../images/homepage/wave3.svg'
 import Mobile1 from '../../../images/homepage/mobile1.png'
 import Mobile2 from '../../../images/homepage/mobile2.png'
+import Download from '../../../images/homepage/download.svg'
+import PlayStore from '../../../images/homepage/playstore.svg'
+import AppStore from '../../../images/homepage/appstore.svg'
+import  Hands from '../../../images/homepage/hands.svg'
+import Speed from '../../../images/homepage/speed.svg'
+import Realable from '../../../images/homepage/realability.svg'
 export const HomeSectionFour =()=>{
 
 
@@ -15,10 +21,53 @@ export const HomeSectionFour =()=>{
                     <img className="mobile1" src={Mobile1} />
                     <img className='mobile2' src={Mobile2} />
                 </div>
-                <div className="rightCol"></div>
+                <div className="rightCol">
+                    <div className="title">
+                        يمكنك ايضا تحميل التطبيق الخاص بنا من المتجر
+                        <img src={Download} />
+                    </div>
+                    <div className="playStore">
+                        <p>For Android</p>
+                        <div className="playStoreLogo">
+                            <img src={PlayStore}/>
+                            <div className="playStoreText">
+                                GETIT ON
+                                <span>Google Play</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="appStore">
+                        <p>For IOS</p>
+                        <div className="appStoreLogo">
+                            <img src={AppStore}/>
+                            <div className="appStoreText">
+                                Download on the 
+                                <span>App Store</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="footer">
-
+              <img src={Speed} id="speed"/>
+              <img src={Realable} id ="realable" />
+              <img src={Hands} id="hands" />
+              <div className="speed">
+                  السرعة
+              </div>
+              <div className='line1Container'>
+                 <div className="line1"></div>
+              </div>
+             
+              <div className="realable">
+                  الثقة
+              </div>
+              <div className='line2Container'>
+                 <div className="line2"></div>
+               </div>  
+              <div className="hands">
+                  الاحترافية
+              </div>
             </div>
         </div>
     )
